ffd7b0309118a9db2cf28e723226e7b9cc6dfefb
Hrishikesh Yadav <93283437+Hrishikesh332@users.noreply.github.com>
2024-11-12 13:14:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, flask
+from flask import Flask, render_template, request, jsonify
 import os
 from dotenv import load_dotenv
 from twelvelabs import TwelveLabs
